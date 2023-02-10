9953ab41e3751cc741805cf1f56239ee07ca425b
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-02-10 21:04:35 +0100

    fix astar

--- codeVisualizer/algorithms/aStar.py
+++ codeVisualizer/algorithms/aStar.py
@@ -9,12 +9,12 @@ class AStar:
     def __init__(self, row, col):
         self.visit = {(row, col)}
         self.directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        self.pq = [(float("inf"), row, col)]
+        self.pq = [(float("inf"), 0, row, col)]
         heapq.heapify(self.pq)
 
     def run(self):
         while self.pq:
-            cost, row, col = heapq.heappop(self.pq)
+            cost, steps, row, col = heapq.heappop(self.pq)
             time.sleep(0.1)
             pygame.display.update()
             for dr, dc in self.directions:
@@ -31,5 +31,5 @@ class AStar:
                         rect = pygame.Rect(c * settings.blocksize, r * settings.blocksize, settings.blocksize, settings.blocksize)
                         pygame.draw.rect(settings.SCREEN, settings.light_GREEN, rect)
                         manhattan_distance = abs(settings.RED_COOR[0][1] - r) + abs(settings.RED_COOR[0][0] - c)
-                        heapq.heappush(self.pq, (manhattan_distance, r, c))
+                        heapq.heappush(self.pq, (manhattan_distance+steps, steps+1, r, c))
                         self.visit.add((r, c))
